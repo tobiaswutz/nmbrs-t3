6a99b816a1afa670a99ede2163a726b60ad3de2d
Tobias <tobiaswutz22@gmail.com>
2023-03-04 18:20:41 +0100

    added layout

---
+++ src/hoc/withAuth.jsx
@@ -0,0 +1,23 @@
+// @ts-nocheck
+import { useSession } from "next-auth/react";
+import Router from "next/router";
+
+const withAuth = (WrappedComponent) => {
+  const Wrapper = (props) => {
+    const { data: session, status } = useSession();
+    if (status === "loading") return <p>Loading...</p>;
+    if (!session) {
+      Router.replace("/auth/login");
+      return null;
+    }
+    return <WrappedComponent {...props} />;
+  };
+  Wrapper.getInitialProps = async (ctx) => {
+    const componentProps =
+      WrappedComponent.getInitialProps &&
+      (await WrappedComponent.getInitialProps(ctx));
+    return { ...componentProps };
+  };
+  return Wrapper;
+};
+export default withAuth;
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "src/components/Layout";
+
+const Index = () => {
+  return (
+    <DashboardLayout>
+      <div>This page is protected!</div>
+    </DashboardLayout>
+  );
+};
+
+export default Index;
